9a15f9535ad402928ef5d4968c0e082dd6f946a1
Dennis Hoppe <dennis.hoppe@debian-solutions.de>
2012-04-11 13:52:23 +0200

    Fix several issues detected by puppet-lint

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,53 +1,53 @@
 class monit {
-	file { "/etc/default/monit":
+	file { '/etc/default/monit':
 		owner   => root,
 		group   => root,
-		mode    => 0644,
-		alias   => "monit",
+		mode    => '0644',
+		alias   => 'monit',
 		source  => "puppet:///modules/monit/${::lsbdistcodename}/etc/default/monit",
-		notify  => Service["monit"],
-		require => Package["monit"],
+		notify  => Service['monit'],
+		require => Package['monit'],
 	}
 
-	file { "/etc/monit/conf.d":
+	file { '/etc/monit/conf.d':
 		force   => true,
 		purge   => true,
 		recurse => true,
 		owner   => root,
 		group   => root,
-		mode    => 0644,
-		require => Package["monit"],
+		mode    => '0644',
+		require => Package['monit'],
 	}
 
-	file { "/etc/monit/monitrc":
+	file { '/etc/monit/monitrc':
 		owner   => root,
 		group   => root,
-		mode    => 0600,
-		alias   => "monitrc",
+		mode    => '0600',
+		alias   => 'monitrc',
 		content => template("monit/${::lsbdistcodename}/etc/monit/monitrc.erb"),
-		notify  => Service["monit"],
-		require => Package["monit"],
+		notify  => Service['monit'],
+		require => Package['monit'],
 	}
 
-	package { "monit":
+	package { 'monit':
 		ensure => present,
 	}
 
-	service { "monit":
-		enable     => true,
+	service { 'monit':
 		ensure     => running,
+		enable     => true,
 		hasrestart => true,
 		hasstatus  => false,
 		require    => [
-			File["monit"],
-			File["monitrc"],
-			Package["monit"]
+			File['monit'],
+			File['monitrc'],
+			Package['monit']
 		],
 	}
 }
 
 class monit::disabled inherits monit {
-	Service["monit"] {
+	Service['monit'] {
 		enable => false,
 		ensure => stopped,
 	}
--- manifests/service.pp
+++ manifests/service.pp
@@ -2,10 +2,10 @@ define monit::service() {
 	file { "/etc/monit/conf.d/${name}":
 		owner   => root,
 		group   => root,
-		mode    => 0644,
+		mode    => '0644',
 		source  => "puppet:///modules/monit/common/etc/monit/conf.d/${name}",
-		notify  => Service["monit"],
-		require => Package["monit"],
+		notify  => Service['monit'],
+		require => Package['monit'],
 	}
 }
 
