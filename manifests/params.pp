ffd421ece46b77def53756fd4eb6e8394bc67095
Dennis Hoppe <dennis.hoppe@debian-solutions.de>
2012-11-21 16:37:20 +0100

    Remove support for Lenny    - End of Life on 2012-02-06

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,6 +1,6 @@
 class monit::params {
   case $::lsbdistcodename {
-    'lenny', 'squeeze', 'maverick', 'natty': {
+    'squeeze', 'maverick', 'natty': {
     }
     default: {
       fail("Module ${module_name} does not support ${::lsbdistcodename}")
